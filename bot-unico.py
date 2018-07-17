8f7e097c9de2087fba403c9b27aff4ed98914021
Jose Sebastián Canós <ep3p@users.noreply.github.com>
2018-07-17 16:17:43 +0200

    Update bot-unico.py

--- bot-unico.py
+++ bot-unico.py
@@ -17,21 +17,21 @@ class MyStreamListener(tweepy.StreamListener):
     def on_status(self, status):
         if (not hasattr(status, 'retweeted_status')
             and not status.in_reply_to_screen_name):
-                matched = pattern.match(status.text)
-                if matched:
-                    variacion = []
-                    variacion.append('Sí, eres ' +  matched.groups()[0])
-                    variacion.append('Sí cariño, ' +  matched.groups()[0])
-                    variacion.append('Sí corazón, ' +  matched.groups()[0])
-                    variacion.append('Sí. ' +  matched.groups()[0].title())
-                    reply = '{4} únic{2}. https://twitter.com/{0}/status/{3}'.format(status.user.screen_name,
-                                                                                     matched.groups()[0],
-                                                                                     matched.groups()[2].lower(),
-                                                                                     status.id,
-                                                                                     random.choice (variacion))
-                    api.update_status(status=reply, in_reply_to_status_id=status.id)
+            matched = pattern.match(status.text)
+            if matched:
+                articulo = matched.groups()[0]
+                genero = matched.groups()[2].lower()
+                variacion = []
+                variacion.append('Sí, eres {} únic{}.'.format(articulo, genero))
+                variacion.append('Sí cariño, {} únic{}.'.format(articulo, genero))
+                variacion.append('Sí corazón, {} únic{}.'.format(articulo, genero))
+                variacion.append('Sí. {} únic{}.'.format(articulo.title(), genero))
+                reply = '{0} https://twitter.com/{1}/status/{2}'.format(random.choice(variacion),                                                                            
+                                                                        status.user.screen_name,
+                                                                        status.id)
+                api.update_status(status=reply, in_reply_to_status_id=status.id)
 
-myStreamListener = MyStreamListener()
-myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
+myListener = MyStreamListener()
+myStream = tweepy.Stream(auth = api.auth, listener=myListener)
 
 myStream.filter(track=['unico', 'unica', 'único', 'única'], async=True)
